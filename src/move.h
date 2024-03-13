0d3d634bf6792cb7ad4a5c8122062135ad2c2b00
mohad12211 <mohammedkaabi64@gmail.com>
2024-03-13 17:18:50 +0300

    Fix windows build by removing raylib header (it wasn't necessary)

--- src/move.h
+++ src/move.h
@@ -1,7 +1,6 @@
 #ifndef MOVE_H
 #define MOVE_H
 
-#include <raylib.h>
 #include <stdbool.h>
 
 typedef struct {
@@ -10,7 +9,5 @@ typedef struct {
 } Move;
 
 bool MoveIsNull(Move move);
-Vector2 MoveStartVector(Move move);
-Vector2 MoveEndVector(Move move);
 
 #endif // MOVE_H
